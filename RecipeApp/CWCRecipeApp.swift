1e9974746d39ddecf60a67cfd3059db0266d42b1
GustavSoderberg <gustav.soderberg@outlook.com>
2022-04-01 20:53:18 +0200

    Initial Commit

---
+++ RecipeApp/CWCRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CWCRecipeApp.swift
+//  CWCRecipeApp
+//
+//  Created by Gustav Söderberg on 2022-03-25.
+//
+
+import SwiftUI
+
+@main
+struct CWCRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RecipeListView()
+        }
+    }
+}
